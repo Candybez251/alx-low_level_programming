7080559ea552a49ad1a49d2e856e87074faa0565
kalkidanlemma <kalkidanblemma@gmail.com>
2023-02-16 14:05:31 -0800

    5printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+include < studio.h >
+/**
+ * main - entry point
+ * Description: prints out a message
+ * Return: 0
+ */
+
+int main(void)
+{         printf("with proper grammar, but the outcome is a piece of art,"0);
+return (0);
+}
