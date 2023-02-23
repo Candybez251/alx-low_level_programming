1bd2215e0cd31802156fc07acdc38957cd5d41b4
kalkidanlemma <kalkidanblemma@gmail.com>
2023-02-23 11:31:10 -0800

    isuuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - function for checking uppercase character
+ * @c: integer variable argument
+ * Return: return 1 if true but 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+}
