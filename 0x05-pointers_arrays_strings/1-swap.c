c9f2b2fc80baaa2d070766eaf4ad0e8e299581dc
kalkidanlemma <kalkidanblemma@gmail.com>
2023-02-27 11:59:36 -0800

    swap 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main h"
+
+/**
+ * swap_int - to swap a and b
+ * @a: number to swap
+ * @b: number to swap
+ */
+
+void swap_int(int *a, int *b);
+{
+	int n;
+
+	*a = n;
+	*a = *b;
+	*b = m;
+}
+
+
