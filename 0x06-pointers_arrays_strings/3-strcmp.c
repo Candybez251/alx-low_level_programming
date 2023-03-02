cf7aac33ee823424776dcdf8bda2a10b53a86a90
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-02 15:04:57 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] == s2[i])
-			break;
+	do {
+		s1 = s1 + 1;
+		s2 = s2 + 1;
 	}
+	while ((*s1 == *s2) && (*s1 && *s2))
+		;
 	return (*s1 - *s2);
 }
