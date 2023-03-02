0e327cb0ed02b57a29c27adade646355cbe4319e
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-02 15:20:28 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,13 @@ void reverse_array(int *a, int n)
 	int i, temp;
 
 	i = 0;
+	n = n - 1;
 	while (i <= a[i])
 	{
-		n = n - 1;
 		temp = a[i];
 		a[i] = a[n];
 		a[n] = temp;
 		i++;
+		n--;
 	}
 }
