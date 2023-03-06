7460b41fc65ba0b8ef8b1bbbdd5e73859019fdb2
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-06 08:47:21 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
+	i = 0;
 	while (n > 0)
 	{
 		dest[i] = src[i];
-		--n;
-		++i;
+		n--;
+		i++;
 	}
 	return (dest);
 }
