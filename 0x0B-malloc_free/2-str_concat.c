4622f77e5bb559f2b5bae970aac230354a170121
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-14 17:05:57 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,8 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i] || s2[2])
-	{
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
-		i++;
-	}
 
 	concat = malloc(len * sizeof(char));
 
