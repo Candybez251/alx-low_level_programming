13728b24bb0fe151713505e438f214fb26e28c4e
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-16 18:53:11 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,17 +6,18 @@
  * @min: starting point of the array
  * @max: end of the array
  * Return: null if min is greater than max and Null if arr fails.
- * Else return arr
  */
 
 int *array_range(int min, int max)
 {
-	int *arr, i = 0, arrSize;
+	int *arr, i, arrSize;
 
 	if (min > max)
 		return (NULL);
+
 	arrSize = max - min + 1;
 	arr = malloc(arrSize * sizeof(int));
+
 	if (arr == NULL)
 		return (NULL);
 	while (i < arrSize)
