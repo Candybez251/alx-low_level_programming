8118c4a3e3d120da7e3085cb54fd897e36d83780
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-16 19:10:04 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,7 +10,7 @@
 
 int *array_range(int min, int max)
 {
-	int *array, index, size;
+	int *array, i, size;
 
 	if (min > max)
 		return (NULL);
@@ -22,8 +22,8 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0; index < size; index++)
-		array[index] = min++;
+	for (i = 0; i < size; i++)
+		array[i] = min++;
 
 	return (array);
 }
