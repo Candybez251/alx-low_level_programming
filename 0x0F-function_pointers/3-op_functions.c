caa208ebcf866ea3d6649968bf4c94cc27d95210
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-22 15:15:42 -0700

    task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns a sum
+ * @a: 1st number
+ * @b: 2nd num
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns division
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: remainder of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
