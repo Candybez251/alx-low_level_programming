d58828539b58b0583ffff2e9bb81380efbb9424d
kalkidanlemma <kalkidanblemma@gmail.com>
2023-03-23 16:39:01 -0700

    fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(va, int));
 
 		if (i != (n - 1))
-			printf("%c ", *separator);
+			printf("%c", *separator);
 	}
 	printf("\n");
 
