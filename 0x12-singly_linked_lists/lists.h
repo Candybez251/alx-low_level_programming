b94cf68755b52f86aaa4173f86e13492092ab2b1
Candybez251 <kalkidanblemma@gmail.com>
2023-03-30 14:12:49 -0700

    task0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct linkedlist - C-Singly linked lists
+ * @str: string
+ * @len: the size of the string
+ * @next: this points to the next node
+ * Description: singly linked list
+ */
+
+typedef struct linkedlist
+{
+	struct linkedlist *next;
+	unsigned int len;
+	char *str;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
